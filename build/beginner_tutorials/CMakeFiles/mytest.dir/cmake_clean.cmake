d46e85cb3294054ee8bf7c4167a1e9f66dfe3eb9
marc <weihang.che@gmail.com>
2018-06-14 13:14:17 +0200

    ros init

---
+++ build/beginner_tutorials/CMakeFiles/mytest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mytest.dir/src/mytest.cpp.o"
+  "/home/marc/catkin_ws/devel/lib/beginner_tutorials/mytest.pdb"
+  "/home/marc/catkin_ws/devel/lib/beginner_tutorials/mytest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mytest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/beginner_tutorials/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+  "/home/marc/catkin_ws/devel/lib/beginner_tutorials/talker.pdb"
+  "/home/marc/catkin_ws/devel/lib/beginner_tutorials/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
